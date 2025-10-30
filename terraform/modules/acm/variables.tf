5ef77a523d4e41be839bc3e5882328f23e57a698
Ilyas Rufai <rufaiilyas@gmail.com>
2025-10-30 11:16:50 +0100

    init commit

---
+++ terraform/modules/acm/variables.tf
@@ -0,0 +1,9 @@
+variable "domain" {
+  description = "Domain name for the certificate"
+  type        = string
+}
+
+variable "zone_id" {
+  description = "Route53 hosted zone ID"
+  type        = string
+}
